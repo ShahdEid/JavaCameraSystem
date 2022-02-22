68ab3364f0d99eee2630ff686c5df1ad30b01dc0
Lukasz Lipka <P2624016@my365.dmu.ac.uk>
2022-02-22 20:31:31 +0000

    Added find method for clsStock

--- CameraClasses/clsStock.cs
+++ CameraClasses/clsStock.cs
@@ -10,10 +10,101 @@ namespace CameraClasses
 {
     public class clsStock
     {
-        public DateTime DateAdded { get; set; }
-        public int StockId { get; set; }
-        public string StockName { get; set; }
-        public int StockQuantity { get; set; }
-        public string StockType { get; set; }
+        //StockId private member variable
+        private Int32 mStockId;
+        //StockId public property
+        public Int32 StockId 
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStockId;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStockId = value;
+            }
+        }
+
+        //DateAdded private member variable
+        private DateTime mDateAdded;
+        //DateAdded public property
+        public DateTime DateAdded
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mDateAdded;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mDateAdded = value;
+            }
+        }
+
+        //StockName private member variable
+        private string mStockName;
+        //StockName public property
+        public string StockName
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStockName;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStockName = value;
+            }
+        }
+
+        //StockQuantity private member variable
+        private Int32 mStockQuantity;
+        //StockQuantity public property
+        public Int32 StockQuantity
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStockQuantity;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStockQuantity = value;
+            }
+        }
+
+        //StockType private member variable
+        private string mStockType;
+        //StockType public property
+        public string StockType
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStockType;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStockType = value;
+            }
+        }
+
+        public bool Find(int stockId)
+        {
+            //set the private data members to the test data value
+            mStockId = 16;
+            mDateAdded = Convert.ToDateTime("22/02/2022");
+            mStockName = "Canon 6D Mk II";
+            mStockQuantity = 12;
+            mStockType = "DSLR Camera";
+            //always return true
+            return true;
+        }
     }
 }
