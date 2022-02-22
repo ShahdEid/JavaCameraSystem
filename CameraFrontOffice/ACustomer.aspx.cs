70025e23217d06f3acac28a84aa20f07388b4d9d
ShahdEid <p2625596@my365.dmu.ac.uk>
2022-02-22 18:25:27 +0000

    Added presentation layer for customer class and a customer viewer web page

--- CameraFrontOffice/ACustomer.aspx.cs
+++ CameraFrontOffice/ACustomer.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CameraClasses;
 
 public partial class ACustomer : System.Web.UI.Page
 {
@@ -14,6 +15,12 @@ public partial class ACustomer : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
-
+        //create a new instance of clsCustomer
+        clsCustomer ACustomer = new clsCustomer();
+        ACustomer.CustomerLName = txtLastName.Text;
+        //store the customer in the session object
+        Session["ACustomer"] = ACustomer;
+        //redirect to the viewer page
+        Response.Redirect("CustomerViewer.aspx");
     }
 }
