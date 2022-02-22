7a7eede4403ae367922547c2e95687fb6b4599b7
p2622948 <p2622948@GWHXX473WIN017W>
2022-02-22 11:19:51 +0000

    clsstaff errror fixed aswell

---
+++ CameraClasses/clsStaff.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data;
+
+namespace CameraClasses
+{
+    public class clsStaff
+    {
+        public string StaffName { get;  set; }
+        public int StaffId { get;  set; }
+        public DateTime DOB { get;  set; }
+        public DateTime DateAdded { get;  set; }
+        public int StaffPhoneNo { get;  set; }
+        public string PostCode { get;  set; }
+        public string HouseNo { get; set; }
+        public string Street { get;  set; }
+    }
+}
--- CameraFrontOffice/JavaStaff.aspx.cs
+++ CameraFrontOffice/JavaStaff.aspx.cs
@@ -4,22 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CameraClasses;
 
 public partial class JavaStaff : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-    {/*
+    {
         //cretae an instance of clsStaff
         clsStaff JavaStaff = new clsStaff();
         //get the data from the session objec
         JavaStaff = (clsStaff)Session["JavaStaff"];
         //display the house number for this entry
         Response.Write(JavaStaff.HouseNo);
-        */
+        
     }
 
     protected void btnOk_Click(object sender, EventArgs e)
-    {/*
+    {
         //An instamce og clsStaff
         clsStaff JavaStaff = new clsStaff();
         //capture the houuse No
@@ -28,6 +29,6 @@ public partial class JavaStaff : System.Web.UI.Page
             Session["JavaStaff"] = JavaStaff;
         //redirect to the viewer page
         Response.Redirect("JavaStaff.aspx");
-        */
+        
     }
 }
