f282c0dd6b7957f0bf0fa39335bf4e0c26c01672
p2622948 <p2622948@GWHXX473WIN017W>
2022-03-25 09:42:27 +0000

    k

--- CameraFrontOffice/JavaStaffDelete.aspx.cs
+++ CameraFrontOffice/JavaStaffDelete.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using CameraClasses;
-using Camera_Testing;
+
 
 public partial class JavaStaffDelete : System.Web.UI.Page
 {
---
+++ dbo.tblStaff.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[tblStaff] ON
+INSERT INTO [dbo].[tblStaff] ([StaffId], [StaffDOB], [StaffName], [StaffPhoneNo], [StaffHouseNo], [StaffStreet], [StaffPostCode], [StaffDateAdded]) VALUES (1, 170102, N'John', N'07459317745', N'1', N'Brabazon', N'Le25hb', N'Leic', N'2022-03-25')
+SET IDENTITY_INSERT [dbo].[tblStaff] OFF
